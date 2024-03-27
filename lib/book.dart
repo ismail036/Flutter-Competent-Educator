e7d524b101f94196cb8e6dac10489052eda659f7
ismail036 <90846601+ismail036@users.noreply.github.com>
2024-03-27 22:30:22 +0300

    .

---
+++ lib/book.dart
@@ -0,0 +1,12 @@
+
+
+class Book{
+  String imgLink;
+  String name;
+  String desc;
+  bool isFavorite;
+  String detailPageLink;
+
+  Book(this.imgLink,this.name,this.desc,this.isFavorite,this.detailPageLink);
+
+}
